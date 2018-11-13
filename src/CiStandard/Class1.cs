dc46732dd7b995bf8ee8ceeda07ee4610612f096
Joan Caron <caronjoan@gmail.com>
2018-11-13 17:28:48 +0100

    Update src/CiStandard/Class1.cs

--- src/CiStandard/Class1.cs
+++ src/CiStandard/Class1.cs
@@ -4,6 +4,13 @@ namespace CiStandard
 {
     public static class Class1
     {
+
+        /// <summary>
+        /// Query if this object is true.
+        /// </summary>
+        /// <returns>
+        /// True if true, false if not.
+        /// </returns>
         public static bool IsTrue() => true;
     }
 }
